032dd4da75baf2c59c3d2e5a50c8501d7a52f315
= <=>
2024-07-10 23:54:16 -0400

    start

--- start_jenkins.sh
+++ start_jenkins.sh
@@ -1,4 +1,4 @@
-# sudo chown -R 1000:1000 /home/carlos/git/jenkins/home
+# sudo chown -R 1000:1000 /home/sixtolio/git/jenkins/homecito
 docker context use default
 docker rm -f jblueQ
-docker run -d --name=jblueQ -p 8091:8080 -p 50000:50000 -v /home/carlos/git/jenkins/homecito:/var/jenkins_home jenkins-cf:v4 
+docker run -d --name=jblueQ -p 8091:8080 -p 50000:50000 -v /home/sixtolio/git/jenkins/homecito:/var/jenkins_home jenkins-cf:v4 
